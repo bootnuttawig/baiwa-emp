6c57a9eb2b1141637ddc671c0d98e1eefc457dc6
Natdanai <natdanai_thaiyala@live.com>
2018-03-12 12:31:23 +0700

    Update Code Rename Project

---
+++ src/main/resources/static/asset/vendors/AdminLTE-2.4.3/js/app.js
@@ -0,0 +1,7 @@
+if (typeof jQuery === "undefined") {
+    throw new Error("AdminLTE requires jQuery");
+}
+var AdminLTEOptions;
+(function ($, jQuery, AdminLTEOptions) {
+//// original code goes here
+})(jQuery, jQuery, AdminLTEOptions); 
